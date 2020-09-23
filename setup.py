19cdc5f3e827fee1b4a456dfbd1419777aa76356
lightspeed393 <lightspeed393@protonmail.com>
2020-09-23 11:16:40 -0700

    Change to electrum-spacecoin.desktop

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if platform.system() in ['Linux', 'FreeBSD', 'DragonFly']:
         else:
             usr_share = os.path.expanduser('~/.local/share')
     data_files += [
-        (os.path.join(usr_share, 'applications/'), ['electrum-zcash.desktop']),
+        (os.path.join(usr_share, 'applications/'), ['electrum-spacecoin.desktop']),
         (os.path.join(usr_share, icons_dirname), ['icons/electrum-spacecoin.png'])
     ]
 
